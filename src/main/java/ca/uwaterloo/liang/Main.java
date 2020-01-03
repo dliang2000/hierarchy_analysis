964a52e428feee1fc082f671f6bc1920cf8255d2
dliang2000 <q8liang@uwaterloo.ca>
2020-01-03 14:57:28 -0500

    transformer update

--- src/main/java/ca/uwaterloo/liang/Main.java
+++ src/main/java/ca/uwaterloo/liang/Main.java
@@ -11,10 +11,54 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		 PackManager.v().getPack("jtp").add(
-			     new Transform("jtp.myTransform", new BodyTransformer() {
-			       protected void internalTransform(Body body, String phase, Map options) {
-			         new LocalMustNotAliasAnalysis(new ExceptionalUnitGraph(body));
+		 PackManager.v().getPack("wjtp").add(
+			     new Transform("wjtp.myTransform", new SceneTransformer() {
+			       protected void internalTransform(String phaseName, Map options) {
+			  		 // after process the entire directory, get the active hierarchy, with all the classes loaded onto scene. 
+			  		 Hierarchy hierarchy = Scene.v().getActiveHierarchy();
+			  		 
+			  		 Set<SootClass> completable_candidates = new HashSet<SootClass>();
+			  		 String classname = null;
+			  		 
+			  		 // Read in the classes with missed methods coverage
+			  		 String csv_file = "commons_MATH_3_6_1_missing_methods.csv";
+			  		 ClassLoader classLoader = new Main().getClass().getClassLoader();
+			  		 File file = new File(classLoader.getResource(csv_file).getFile());
+			  		 
+			  		 System.out.println("File Found : " + file.exists());
+			  		 
+			  		 BufferedReader reader;
+			  		 try {
+						reader = new BufferedReader(new FileReader(file));
+						String line = null;
+				  		while((line = reader.readLine()) != null){
+				  			 classname = line;
+				  			 SootClass sc = Scene.v().loadClassAndSupport(classname);
+				  			 System.out.println(classname);
+				  			 sc.setApplicationClass();
+				  			 
+				  			 List<SootClass> l = hierarchy.getSuperclassesOf(sc);
+				  			 if (l.size() == 1)
+				  				 // when the size of l is 1, it means the superclass of sc is java.lang.Object.
+				  				 continue;
+				  			 SootClass superclass = l.get(0);
+				  			 // System.out.println("Package name: " + superclass.getPackageName());
+				  			 // System.out.println("Class name: " + superclass.getName());
+				  		   
+				  			 if (ifHaveMultipleDirectSubClasses(superclass, hierarchy)) {
+				  				 completable_candidates.add(superclass);
+				  			 }
+				  		}
+				  		for (SootClass s: completable_candidates) {
+							 System.out.println("Class name: " + s.getName());
+						}
+			  		 } catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+			  		 } catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+			  		 }
 			       }
 			     }));
 		 String class_path = File.separator + "home" + File.separator + "daveroar" + File.separator + 
@@ -25,43 +69,6 @@ public class Main {
 		 Options.v().set_verbose(true);
 		 Options.v().set_soot_classpath(class_path);
 		 soot.Main.main(args);
-		 
-		 // after process the entire directory, get the active hierarchy, with all the classes loaded onto scene. 
-		 Hierarchy hierarchy = Scene.v().getActiveHierarchy();
-		 
-		 List<SootClass> completable_candidates = new ArrayList<SootClass>();
-		 String classname = null;
-		 
-		 // Read in the classes with missed methods coverage
-		 String csv_file = "commons_MATH_3_6_1_missing_methods.csv";
-		 ClassLoader classLoader = new Main().getClass().getClassLoader();
-		 File file = new File(classLoader.getResource(csv_file).getFile());
-		 
-		 System.out.println("File Found : " + file.exists());
-		 
-		 BufferedReader reader = new BufferedReader(new FileReader(file));
-		 String line = null;
-		 while((line = reader.readLine()) != null){
-			 classname = line;
-			 SootClass sc = Scene.v().loadClassAndSupport(classname);
-			 System.out.println(classname);
-			 
-			 List<SootClass> l = hierarchy.getSuperclassesOf(sc);
-			 if (l.size() == 1)
-				 // when the size of l is 1, it means the superclass of sc is java.lang.Object.
-				 continue;
-			 SootClass superclass = l.get(0);
-			 // System.out.println("Package name: " + superclass.getPackageName());
-			 // System.out.println("Class name: " + superclass.getName());
-		   
-			 if (ifHaveMultipleDirectSubClasses(superclass, hierarchy)) {
-				 completable_candidates.add(superclass);
-			 }
-		 }
-		 
-		 for (SootClass s: completable_candidates) {
-			 System.out.println("Class name: " + s.getName());
-		 }
 	}
 	
 	public static String executeCommand(String command) {
