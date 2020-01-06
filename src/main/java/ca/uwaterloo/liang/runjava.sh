4deab8aa05c06e5be14b9cbc87d56fa785fd9e42
dliang2000 <q8liang@uwaterloo.ca>
2020-01-05 20:14:05 -0500

    first attempt for script

---
+++ src/main/java/ca/uwaterloo/liang/runjava.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+CLASSPATH="$HOME/Graduation_Studies/ThesisWork/OpenSourceProjects/commons-math-MATH_3_6_1/src/main/java"
+cd $CLASSPATH
+shopt -s globstar
+mkdir classes
+javac **/*.java -d \classes
+
+java -cp ca.uwaterloo.liang.Main -process-dir .
