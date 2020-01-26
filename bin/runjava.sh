57256f396fcdffeb77df4243d04f8d504d464f5d
dliang2000 <q8liang@uwaterloo.ca>
2020-01-25 22:52:04 -0500

    candidates in HTML format

--- bin/runjava.sh
+++ bin/runjava.sh
@@ -1,8 +1,24 @@
-#!/bin/bash
-CLASSPATH="$HOME/Graduation_Studies/ThesisWork/OpenSourceProjects/commons-math-MATH_3_6_1/src/main/java"
-cd $CLASSPATH
-shopt -s globstar
-mkdir classes
-javac **/*.java -d \classes
+# CLASSPATH="$HOME/Graduation_Studies/ThesisWork/OpenSourceProjects/commons-math-MATH_3_6_1/src/main/java"
+# cd $CLASSPATH
+# shopt -s globstar
+# mkdir classes
+# javac **/*.java -d \classes
 
-java -cp ca.uwaterloo.liang.Main
+#!/bin/bash
+#under msp everything should be identical on all computers
+MACHINE_SPECIFIC_PATH="$HOME/Graduation_Studies/ThesisWork/OpenSourceProjects"
+
+SOOT_JAR="soot_jar/sootclasses-trunk-jar-with-dependencies.jar"
+CC_PATH="hierarchy-analysis/src/main/java/ca/uwaterloo/liang/Main.java"
+BENCHMARK_PATH="Benchmarks/commons-math-MATH_3_6_1"
+TARGET_PATH="target/classes/"
+
+cd $MACHINE_SPECIFIC_PATH/$BENCHMARK_PATH
+# touch is_maven in the benchmark directory
+if [-x is_maven ]; then
+  mvn compile
+fi
+
+
+
+java -cp $SOOT_JAR:$CC_PATH $MACHINE_SPECIFIC_PATH/$BENCHMARK_PATH/$TARGET_PATH
