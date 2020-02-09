b4a3aa1900fa47e61ad31bd4e95a015416b5dc2d
dliang2000 <q8liang@uwaterloo.ca>
2020-02-09 17:26:37 -0500

    more scripts

---
+++ bin/runjava_commons_collections.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#under msp everything should be identical on all computers
+MACHINE_SPECIFIC_PATH="$HOME/Graduation_Studies/ThesisWork/OpenSourceProjects"
+
+SOOT_JAR="$MACHINE_SPECIFIC_PATH/soot_jar/sootclasses-trunk-jar-with-dependencies.jar"
+JAVA_PATH="$MACHINE_SPECIFIC_PATH/hierarchy-analysis/target/classes"
+CC_CLASS="ca.uwaterloo.liang.Main"
+BENCHMARK_PATH="$MACHINE_SPECIFIC_PATH/Benchmarks/commons-collections-collections-4.3"
+TARGET_PATH="target/classes"
+
+cd $BENCHMARK_PATH
+
+# touch is_maven in the benchmark directory to indicate that a benchmark is mvn
+if [ -a is_maven ]; then
+  echo "it is a maven project"
+  mvn compile
+fi
+
+java -cp $SOOT_JAR:$JAVA_PATH $CC_CLASS $BENCHMARK_PATH/$TARGET_PATH `cat benchmark_class_path` commons_collections_collections_4.3_missing_methods.csv
