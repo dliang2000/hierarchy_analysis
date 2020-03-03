301cd1c5f3c856df34ba4f919308f4af17edc3dc
dliang2000 <q8liang@uwaterloo.ca>
2020-03-03 01:52:15 -0500

    add javacc and partially add for jgrapht

--- vagrant_setup/bootstrap.sh
+++ vagrant_setup/bootstrap.sh
@@ -25,6 +25,14 @@ case $(id -u) in
 	  cd benchmarks
     mkdir csv
   echo "In benchmarks folder"
+
+  # # 3.3 runs fine
+	# wget http://archive.apache.org/dist/commons/math/source/commons-math3-3.6.1-src.tar.gz
+	# tar xzvf commons-math3-3.6.1-src.tar.gz
+	# ( cd commons-math3-3.6.1-src; mvn test )
+  #
+
+  # benchmark 1: jsoup-1.10.1
   # jsoup runs fine, jacoco .xml report generated successfully
 	wget https://github.com/jhy/jsoup/archive/jsoup-1.10.1.tar.gz
 	tar xzvf jsoup-1.10.1.tar.gz
@@ -33,12 +41,8 @@ case $(id -u) in
   ( cd jsoup-jsoup-1.10.1; java -jar /vagrant/shared/jacoco-0.8.5/lib/jacococli.jar report --classfiles ./target/classes/ --xml ./jsoup_jsoup_1.10.1_coverage.xml )
   ( cd jsoup-jsoup-1.10.1; python3 /vagrant/shared/python_scripts/jsoup_jsoup_1.10.1_processing.py )
 
-  # # 3.3 runs fine
-	# wget http://archive.apache.org/dist/commons/math/source/commons-math3-3.6.1-src.tar.gz
-	# tar xzvf commons-math3-3.6.1-src.tar.gz
-	# ( cd commons-math3-3.6.1-src; mvn test )
-  #
-  # commons-lang3-3.9
+  # benchmark 2: commons-lang3-3.9
+  # commons-lang3-3.9 runs fine, jacoco .xml report generated successfully
   wget http://archive.apache.org/dist/commons/lang/source/commons-lang3-3.9-src.tar.gz
 	tar xzvf commons-lang3-3.9-src.tar.gz
   ( cd commons-lang3-3.9-src; patch pom.xml -i /vagrant/shared/patch/patchfile_commons_lang3_3.9.patch )
@@ -53,6 +57,7 @@ case $(id -u) in
     # ( cd apache-jmeter-5.2.1; ./gradlew -x :src:dist-check:batchTestRedirectionPolicies test)
     # ( cd apache-jmeter-5.2.1; ./gradlew -x :src:dist-check:batchTestRedirectionPolicies jacocoReport)
 
+    # benchmark 3: joda-time-2.10.5
     # joda-time-2.10.5 runs fine, jacoco .xml report generated successfully
     wget https://github.com/JodaOrg/joda-time/archive/v2.10.5.tar.gz
     tar xzvf v2.10.5.tar.gz
@@ -61,7 +66,8 @@ case $(id -u) in
     ( cd joda-time-2.10.5; java -jar /vagrant/shared/jacoco-0.8.5/lib/jacococli.jar report --classfiles ./target/classes/ --xml ./joda_time_2.10.5_coverage.xml )
     ( cd joda-time-2.10.5; python3 /vagrant/shared/python_scripts/joda_time_2.10.5_processing.py )
 
-    # findbugs-3.0.1
+    # benchmark 4: findbugs-3.0.1
+    # findbugs-3.0.1 runs fine, jacoco .xml report generated successfully
     wget https://sourceforge.net/projects/findbugs/files/findbugs/3.0.1/findbugs-3.0.1-source.zip
     unzip findbugs-3.0.1-source.zip
     ( cd findbugs-3.0.1; patch pom.xml -i /vagrant/shared/patch/patchfile_findbugs_3.0.1.patch )
@@ -69,13 +75,32 @@ case $(id -u) in
     ( cd findbugs-3.0.1; java -jar /vagrant/shared/jacoco-0.8.5/lib/jacococli.jar report --classfiles ./target/classes/ --xml ./findbugs_3.0.1_coverage.xml )
     ( cd findbugs-3.0.1; python3 /vagrant/shared/python_scripts/findbugs_3.0.1_processing.py )
 
-    # gson-2.8.5
+    # benchmark 5: gson-2.8.5
+    # gson-2.8.5 runs fine, jacoco .xml report generated successfully
     wget https://github.com/google/gson/archive/gson-parent-2.8.5.tar.gz
     tar xzvf gson-parent-2.8.5.tar.gz
     ( cd gson-gson-parent-2.8.5; patch pom.xml -i /vagrant/shared/patch/patchfile_gson_2.8.5.patch )
     ( cd gson-gson-parent-2.8.5; mvn clean test )
     ( cd gson-gson-parent-2.8.5; java -jar /vagrant/shared/jacoco-0.8.5/lib/jacococli.jar report --classfiles ./target/classes/ --xml ./gson_2.8.5_coverage.xml )
     ( cd gson-gson-parent-2.8.5; python3 /vagrant/shared/python_scripts/gson_2.8.5_processing.py )
+
+    # benchmark 6: javacc-7.0.5
+    # javacc-7.0.5 runs fine, jacoco .xml report generated successfully
+    wget https://github.com/javacc/javacc/archive/7.0.5.tar.gz
+    tar xzvf 7.0.5.tar.gz
+    ( cd javacc-7.0.5; patch pom.xml -i /vagrant/shared/patch/patchfile_javacc_7.0.5.patch )
+    ( cd javacc-7.0.5; mvn clean test )
+    ( cd javacc-7.0.5; java -jar /vagrant/shared/jacoco-0.8.5/lib/jacococli.jar report ./target/coverage-reports/jacoco-ut.exec --classfiles ./target/classes/ --xml ./javacc_7.0.5_coverage.xml )
+    ( cd javacc-7.0.5; python3 /vagrant/shared/python_scripts/javacc_7.0.5_processing.py )
+
+    # benchmark 6: jgrapht-1.3.1
+    #
+    wget https://github.com/jgrapht/jgrapht/archive/jgrapht-1.3.1.tar.gz
+    tar xzvf jgrapht-1.3.1.tar.gz
+    ( cd jgrapht-jgrapht-1.3.1; patch pom.xml -i /vagrant/shared/patch/patchfile_jgrapht_1.3.1.patch )
+    ( cd jgrapht-jgrapht-1.3.1; mvn clean test )
+    ( cd jgrapht-jgrapht-1.3.1; java -jar /vagrant/shared/jacoco-0.8.5/lib/jacococli.jar report ./target/coverage-reports/jacoco-ut.exec --classfiles ./target/classes/ --xml ./javacc_7.0.5_coverage.xml )
+    ( cd jgrapht-jgrapht-1.3.1; python3 /vagrant/shared/python_scripts/javacc_7.0.5_processing.py )
 
     # guava-28.2
     # wget https://github.com/google/guava/archive/v28.2.tar.gz
@@ -84,11 +109,6 @@ case $(id -u) in
     # ( cd guava-28.2; mvn clean test )
     # ( cd guava-28.2; java -jar /vagrant/shared/jacoco-0.8.5/lib/jacococli.jar report --classfiles ./target/classes/ --xml ./guava_28.2_coverage.xml )
 
-    # tomcat-9.0.31
-    # will work out insert jacoco plugin correctly into ant build.xml file
-    # wget https://github.com/apache/tomcat/archive/9.0.31.tar.gz
-    # tar xzvf 9.0.31.tar.gz
-    # ( cd tomcat-9.0.31/; )
 
 
     ;;
