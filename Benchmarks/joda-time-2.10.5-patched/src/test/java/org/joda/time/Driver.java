59029bdd3f057714fe42405a72326ae69e0d7605
dliang2000 <q8liang@uwaterloo.ca>
2021-02-15 19:57:01 -0500

    add Benchmark joda-time

---
+++ Benchmarks/joda-time-2.10.5-patched/src/test/java/org/joda/time/Driver.java
@@ -0,0 +1,8 @@
+package org.joda.time;
+
+public class Driver {
+	public static void main(String[] argv) {
+		org.joda.time.chrono.TestBuddhistChronology class1 = new org.joda.time.chrono.TestBuddhistChronology("Buddist");
+		class1.testWithZone();
+	}
+}
