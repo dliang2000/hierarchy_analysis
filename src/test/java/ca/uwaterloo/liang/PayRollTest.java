5ec95967035562c917f71e3e71df2c83bde805fb
dliang2000 <q8liang@uwaterloo.ca>
2021-03-26 23:29:52 -0400

    add intra and array for mock testing

--- src/test/java/ca/uwaterloo/liang/PayRollTest.java
+++ src/test/java/ca/uwaterloo/liang/PayRollTest.java
@@ -37,7 +37,22 @@ public class PayRollTest {
     public void testNoEmployees() {
         assertNumberOfPayments(0);
     }
+    
+    @Test
+    public void testNoEmployeesIntra() {
+        List<Employee> employees_intra = new ArrayList<Employee>();
 
+        EmployeeList employeeList_intra = mock(EmployeeList.class);
+        BankService bankService_intra = mock(BankService.class);
+
+        when(employeeList_intra.getAllEmployees()).thenReturn(employees_intra);
+
+        PayRoll payRoll_intra = new PayRoll(employeeList_intra, bankService_intra);
+        
+        int numberOfPayments = payRoll_intra.monthlyPayment();
+        assertEquals(0, numberOfPayments);
+    }
+    
     @Test
     public void testSingleEmployee() {
         employees.add(createTestEmployee("Test Employee", "ID1", 1000));
@@ -74,7 +89,27 @@ public class PayRollTest {
         assertEquals(1000, salaryCaptor.getAllValues().get(0).intValue());
         assertEquals(2000, salaryCaptor.getAllValues().get(1).intValue());
     }
+    
+    @Test
+    public void testAllEmployeesArePaidArray() {
+        Employee[] employee_arr = createEmployees();
+        employees.add(employee_arr[1]);
+        employees.add(employee_arr[2]);
 
+        assertNumberOfPayments(2);
+
+        ArgumentCaptor<String> idCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<Integer> salaryCaptor = ArgumentCaptor.forClass(Integer.class);
+
+        verify(bankService, times(2)).makePayment(idCaptor.capture(), salaryCaptor.capture());
+
+        assertEquals("ID0", idCaptor.getAllValues().get(0));
+        assertEquals("ID1", idCaptor.getAllValues().get(1));
+        assertEquals(1000, salaryCaptor.getAllValues().get(0).intValue());
+        assertEquals(2000, salaryCaptor.getAllValues().get(1).intValue());
+    }
+    
+    
     @Test
     public void testInteractionOrder() {
         String employeeId = "ID0";
@@ -97,5 +132,14 @@ public class PayRollTest {
     private Employee createTestEmployee(String name, String id, int salary) {
         return new Employee(name, id, salary);
     }
-
+    
+    /**
+     * creates an array of four Node instances, mocked by EasyMock.
+     */
+    private Employee[] createEmployees() {
+        Employee employee1 = mock(Employee.class);
+        Employee employee2 = mock(Employee.class);
+        
+        return new Employee[]{employee1, employee2};
+    }
 }
